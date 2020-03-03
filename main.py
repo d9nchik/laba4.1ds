bc203f7c82b0f7232420aa4c05d63fbc8df1dee0
Danylo Halaiko <dan.galaiko@gmail.com>
2020-03-03 14:48:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_data():
+    e_data = []
+    with open("input.txt") as iFile:
+        while True:
+            line = iFile.readline()
+            if not line:
+                break
+            temp = (line[:len(line)] + line[len(line) + 1:]).split()
+            for x in range(len(temp)):
+                temp[x] = int(temp[x])
+            e_data.append(temp)
+            print(line, end='')
+        print()
+    return e_data
